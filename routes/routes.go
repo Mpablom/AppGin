06265e719951ba520639b78ec9a191d202c14ce0
pablo <pablomoya211@gmail.com>
2025-06-01 18:25:58 -0300

    primera prueba de plantillas html en gin

--- routes/routes.go
+++ routes/routes.go
@@ -14,30 +14,14 @@ type Usuario struct {
 var usuarios []Usuario
 
 func SetupRoutes(r *gin.Engine) {
+	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Started!")
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Title":   "Mi App",
+			"Heading": "Hola a todos!",
+			"Message": "Bienvenidos a esta prueba con gin y plantillas html",
+		})
 	})
-
-	r.GET("/saludo/:nombre", func(c *gin.Context) {
-		nombre := c.Param("nombre")
-		c.String(http.StatusOK, "Hola %s!", nombre)
-	})
-
-	r.POST("/usuarios", func(c *gin.Context) {
-		var nuevoUsuario Usuario
-
-		if err := c.BindJSON(&nuevoUsuario); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al decodificar el JSON"})
-			return
-		}
-
-		if nuevoUsuario.Nombre == "" || nuevoUsuario.Email == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Nombre y email son requeridos"})
-			return
-		}
-
-		usuarios = append(usuarios, nuevoUsuario)
-
-		c.JSON(http.StatusOK, gin.H{"mensaje": "Usuario creado exitosamente", "usuario": usuarios})
-	})
+	r.Static("/static", "./static")
 }
